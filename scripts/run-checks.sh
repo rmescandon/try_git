262957bc8ce281edecb9ffe021b4f5612e7790cd
Roberto Mier Escandon <rmescandon@gmail.com>
2021-11-06 12:01:32 +0100

    rectify execution permissions and run-checks script

--- scripts/run-checks.sh
+++ scripts/run-checks.sh
@@ -3,15 +3,13 @@
 
 # Always run in virtual environment. Create it if needed
 if [ -z "$VIRTUAL_ENV" ]; then
-    [ -d "$(dirname "$0")/venv" ] || (cd "$(dirname "$0")" && ./venv.sh) 
+    [ -d "$(dirname "$0")/venv" ] || (cd "$(dirname "$0")" && virtualenv -p python3 venv) 
     # shellcheck disable=SC1091
     cd "$(dirname "$0")" && . venv/bin/activate && cd - > /dev/null
 fi
 
-# Shellcheck over existing scripts in project root
-shellcheck -x -- *.sh
-# Shellcheck over scripts into 'tools' subfolder
-find tools -type f \( -name "*.sh" -o -name "*.bash" -o -name "*.ksh" -o -name "*.bashrc" -o -name "*.bash_profile" -o -name "*.bash_login" -o -name "*.bash_logout" \) -print |
+# Shellcheck over scripts into 'scripts' subfolder
+find scripts -type f \( -name "*.sh" -o -name "*.bash" -o -name "*.ksh" -o -name "*.bashrc" -o -name "*.bash_profile" -o -name "*.bash_login" -o -name "*.bash_logout" \) -print |
 while IFS="" read -r file; do
     shellcheck -x -- "$file"
 done
